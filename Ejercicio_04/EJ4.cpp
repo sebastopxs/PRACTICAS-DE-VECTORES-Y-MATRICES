f96583d681fbc02029f5526d1ce58456226e26f6
Sebastian <jobhuamanrios@gmail.com>
2026-06-20 16:50:41 -0500

    Implement vector scaling in EJ4.cpp
    This program takes a vector size and an scalar input, then multiplies each vector element by the scalar and displays the resulting vector.

---
+++ Ejercicio_04/EJ4.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    float k, v[100];
+
+    cout << "Tamano del vector: ";
+    cin >> n;
+    cout << "Ingrese el escalar (k): ";
+    cin >> k;
+
+    cout << "Ingrese los elementos del vector:\n";
+    for (int i = 0; i < n; i++) {
+        cin >> v[i];
+        v[i] = v[i] * k; 
+    }
+
+    cout << "Vector resultante: ";
+    for (int i = 0; i < n; i++) {
+        cout << v[i] << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
